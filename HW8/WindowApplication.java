411416487a5ca4bfaaa2aa8384c87c028281ffa9
Willis Allstead <willallstead@icloud.com>
2017-05-08 18:29:55 -0700

    added window destroyer

--- HW8/WindowApplication.java
+++ HW8/WindowApplication.java
@@ -10,6 +10,20 @@ public class WindowApplication
 	{
 		JFrame frame = new JFrame("Window Application");
 		frame.setSize(350, 150);
+        frame.addWindowListener(new WindowDestroyer());
 		frame.setVisible(true);
 	}
+}
+
+class WindowDestroyer implements WindowListener
+{
+	public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+	public void windowActivated(WindowEvent e) {}
+	public void windowClosed(WindowEvent e) {}
+	public void windowDeactivated(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {}
+	public void windowOpened(WindowEvent e) {}
 }
