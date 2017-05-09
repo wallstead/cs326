c66ba8d66f5e450658560ac54989e3bae2d1c04e
Willis Allstead <willallstead@icloud.com>
2017-05-08 18:27:39 -0700

    hello world

---
+++ HW8/WindowApplication.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class WindowApplication
+{
+	public static void main (String argv [])
+	{
+		JFrame frame = new JFrame("Window Application");
+		frame.setSize(350, 150);
+		frame.setVisible(true);
+	}
+}
